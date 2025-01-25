caadbc1c18fa629d00238a82be675987f156c407
namishy <evaricating@gmail.com>
2025-01-26 01:06:51 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Phoenixfy";
+const meta = {
+    title: "Namish",
+    description: "I’m Namish Editor_Gamer,currently a student",
+};
+
+const introdata = {
+    title: "I’m Namish",
+    animated: {
+        first: "I love Editing",
+        second: "I code sometimes",
+        third: "I play games",
+    },
+    description: ":)",
+    your_img_url: "https://i.pinimg.com/736x/fe/74/a5/fe74a5a82dbe7ea754cd5bbb1c602b1d.jpg",
+};
+
+const dataabout = {
+    title: "Platforms i'm available on",
+    aboutme: "Discord , Instagram , telegram , youtube ",
+};
+const worktimeline = [{
+        jobtitle: "Adobe",
+        where: "After Effects",
+        date: "2022",
+    },
+    {
+        jobtitle: "Adobe",
+        where: "Premiere Pro",
+        date: "2020",
+    },
+    {
+        jobtitle: "For exporting",
+        where: "Media Encoder",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Adobe After Effects",
+        value: 90,
+    },
+    {
+        name: "Adobe Premiere Pro",
+        value: 85,
+    },
+    {
+        name: "BGMI (PUBG)",
+        value: 80,
+    },
+    {
+        name: "Adobe Illustrator",
+        value: 27,
+    },
+    {
+        name: "Adobe Photoshop",
+        value: 69,
+    },
+];
+
+const services = [{
+        title: "Video Editing",
+        description: "Can do formal and vfx",
+    },
+    {
+        title: "Photo Editing",
+        description: "thumbnails , banners or logos",
+    },
+    {
+        title: "3d Animations",
+        description: "Only when ae e3d",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/736x/3a/23/61/3a236138fd07936df13feb91dd45302e.jpg",
+        description: "Main Instagram",
+        link: "https://instagram.com/p3x.46",
+    },
+    {
+        img: "https://i.pinimg.com/736x/dc/13/5b/dc135b197ec51d59776d58bb2f50dc36.jpg",
+        description: "Private Instagram",
+        link: "https://instagram.com/nami5h",
+    },
+    {
+        img: "https://i.pinimg.com/736x/e9/85/09/e98509c64b55846417320dea415318f3.jpg",
+        description: "Discord server",
+        link: "https://discord.gg/3ZhASTgF36",
+    },
+    {
+        img: "https://i.pinimg.com/736x/e6/ad/24/e6ad243eb5a17f49adccff8ba50f2086.jpg",
+        description: "Discord is px93",
+        link: "#",
+    },
+    {
+        img: "https://i.pinimg.com/736x/fa/1a/32/fa1a32f6666cf46ec1d402930bb913f1.jpg",
+        description: "Backup Instagram",
+        link: "https://instagram.com/namishfr",
+    },
+    {
+        img: "https://i.pinimg.com/736x/20/d4/66/20d466722fad5bf0ca8b9c6301851bd4.jpg",
+        description: "Telegram",
+        link: "https://https://t.me/MANlPULATE",
+    },
+
+    {
+        img: "https://i.pinimg.com/736x/eb/50/4e/eb504e696b1846391673d35acab952c4.jpg",
+        description: "Youtube",
+        link: "https://youtube.com/@p3x_",
+    },
+    {
+        img: "https://i.pinimg.com/736x/20/c0/fa/20c0faf83bf58180a1a1efb85cd1b3a5.jpg",
+        description: "Twitter",
+        link: "https://x.com/phoenixfy_",
+    },
+    {
+        img: "https://i.pinimg.com/736x/c7/92/38/c7923897a994f21c3cec656fa2168393.jpg",
+        description: "Mail Me",
+        link: "mailto:inselycain@yahoo.com",
+    },
+    {
+        img: "https://i.pinimg.com/736x/19/a4/7b/19a47b20d9117abdea70a355373f78b3.jpg",
+        description: "Old Website",
+        link: "https://p3xz.github.io",
+    },
+    {
+        img: "https://i.pinimg.com/736x/36/35/db/3635db3ad43bb971829c228aa0974fa4.jpg",
+        description: "Solo to for a lite version",
+        link: "https://solo.to/p3x",
+    },
+    {
+        img: "https://i.pinimg.com/736x/2d/29/95/2d2995f7c00cfd284d9dd87af6f8ef99.jpg",
+        description: "Spotify",
+        link: "https://open.spotify.com/user/5cy82xgvz8ulnsrrswxqrvmhh",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "inselycain@yahoo.com",
+    YOUR_FONE: "px93",
+    description: "If this doesnt work , go in the socials section search for mail me tab and hit me up there    ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_1ieiv7i",
+    YOUR_TEMPLATE_ID: "template_nqelg6f",
+    YOUR_USER_ID: "Clz0mTqelZXwSot8D",
+};
+
+const socialprofils = {
+    github: "https://github.com/p3xz",
+    instagram: "https://instagram.com/p3x.46",
+    youtube: "https://youtube.com/@p3x_",
+    twitter: "https://x.com/phoenixfy_",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
